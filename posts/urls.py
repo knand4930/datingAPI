ab3964c041844861c41bc8b7dab2e5238c8f0c48
Nand Kishore <nandk@thinkdatalabs.com>
2023-10-17 21:27:18 +0530

    post comment like view share and update delete

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 
-from posts.views import FilePostListAPI, FilePostDestroyAPI, FilePostUpdateAPI, FilePostRetrieveAPI, FilePostPostAPI
+from posts.views import FilePostListAPI, FilePostDestroyAPI, FilePostUpdateAPI, FilePostRetrieveAPI, FilePostPostAPI, \
+    ShareFilePostListAPI, ShareFilePostPostAPI, ShareFilePostRetrieveAPI, ShareFilePostUpdateAPI, \
+    ShareFilePostDestroyAPI, CommentFilePostListAPI, CommentFilePostPostAPI, CommentFilePostRetrieveAPI, \
+    CommentFilePostUpdateAPI, CommentFilePostDestroyAPI, LikeFilePostListAPI, LikeFilePostPostAPI, \
+    LikeFilePostRetrieveAPI, LikeFilePostUpdateAPI, LikeFilePostDestroyAPI, ViewFilePostListAPI, ViewFilePostPostAPI, \
+    ViewFilePostRetrieveAPI, ViewFilePostUpdateAPI, ViewFilePostDestroyAPI, CommentRepliesView
 
 urlpatterns = [
     path('get/', FilePostListAPI.as_view(), name="FilePostListAPI"),
@@ -8,4 +13,30 @@ urlpatterns = [
     path('<uuid:pk>/patch/', FilePostRetrieveAPI.as_view(), name="FilePostRetrieveAPI"),
     path('<uuid:pk>/update/', FilePostUpdateAPI.as_view(), name="FilePostUpdateAPI"),
     path('<uuid:pk>/delete/', FilePostDestroyAPI.as_view(), name="FilePostDestroyAPI"),
+
+    path('share/get/', ShareFilePostListAPI.as_view(), name="ShareFilePostListAPI"),
+    path('share/post/', ShareFilePostPostAPI.as_view(), name="ShareFilePostPostAPI"),
+    path('share/<uuid:pk>/patch/', ShareFilePostRetrieveAPI.as_view(), name="ShareFilePostRetrieveAPI"),
+    path('share/<uuid:pk>/update/', ShareFilePostUpdateAPI.as_view(), name="ShareFilePostUpdateAPI"),
+    path('share/<uuid:pk>/delete/', ShareFilePostDestroyAPI.as_view(), name="ShareFilePostDestroyAPI"),
+
+    path('comment/get/', CommentFilePostListAPI.as_view(), name="CommentFilePostListAPI"),
+    path('comment/post/', CommentFilePostPostAPI.as_view(), name="CommentFilePostPostAPI"),
+    path('comment/<uuid:pk>/patch/', CommentFilePostRetrieveAPI.as_view(), name="CommentFilePostRetrieveAPI"),
+    path('comment/<uuid:pk>/update/', CommentFilePostUpdateAPI.as_view(), name="CommentFilePostUpdateAPI"),
+    path('comment/<uuid:pk>/delete/', CommentFilePostDestroyAPI.as_view(), name="CommentFilePostDestroyAPI"),
+    path('comments/replies/<uuid:parent_comment_id>/', CommentRepliesView.as_view(), name='comment-replies'),
+
+    path('like/get/', LikeFilePostListAPI.as_view(), name="LikeFilePostListAPI"),
+    path('like/post/', LikeFilePostPostAPI.as_view(), name="LikeFilePostPostAPI"),
+    path('like/<uuid:pk>/patch/', LikeFilePostRetrieveAPI.as_view(), name="LikeFilePostRetrieveAPI"),
+    path('like/<uuid:pk>/update/', LikeFilePostUpdateAPI.as_view(), name="LikeFilePostUpdateAPI"),
+    path('like/<uuid:pk>/delete/', LikeFilePostDestroyAPI.as_view(), name="LikeFilePostDestroyAPI"),
+
+    path('view/get/', ViewFilePostListAPI.as_view(), name="ViewFilePostListAPI"),
+    path('view/post/', ViewFilePostPostAPI.as_view(), name="ViewFilePostPostAPI"),
+    path('view/<uuid:pk>/patch/', ViewFilePostRetrieveAPI.as_view(), name="ViewFilePostRetrieveAPI"),
+    path('view/<uuid:pk>/update/', ViewFilePostUpdateAPI.as_view(), name="ViewFilePostUpdateAPI"),
+    path('view/<uuid:pk>/delete/', ViewFilePostDestroyAPI.as_view(), name="ViewFilePostDestroyAPI"),
+
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,14 +1,11 @@
 from rest_framework import permissions, status
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
-
 from posts.models import FilePost, ShareFilePost, CommentFilePost, LikeFilePost, ViewFilePost
 from posts.serializers import (FilePostSerializer, ShareFilePostSerializer, CommentFilePostSerializer,
                                LikeFilePostSerializer, ViewFilePostSerializer, FilePostDataSerializer,
                                FilePostUpdateSerializer)
-
-from rest_framework.generics import ListAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView, RetrieveAPIView, \
-    ListCreateAPIView
+from rest_framework.generics import ListAPIView, UpdateAPIView, DestroyAPIView, RetrieveAPIView, ListCreateAPIView
 
 
 # Create your views here.
@@ -59,3 +56,194 @@ class FilePostDestroyAPI(DestroyAPIView):
                             status=status.HTTP_403_FORBIDDEN)
 
 
+class ShareFilePostPostAPI(ListCreateAPIView):
+    serializer_class = ShareFilePostSerializer
+    queryset = ShareFilePost.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class ShareFilePostListAPI(ListAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = ShareFilePostSerializer
+    queryset = ShareFilePost.objects.all()
+
+
+class ShareFilePostRetrieveAPI(RetrieveAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = ShareFilePostSerializer
+    queryset = ShareFilePost.objects.all()
+
+
+class ShareFilePostUpdateAPI(UpdateAPIView):
+    serializer_class = ShareFilePostSerializer
+    queryset = ShareFilePost.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_update(self, serializer):
+        instance = self.get_object()
+
+        if self.request.user == instance.user:
+            serializer.save()
+        else:
+            return Response({"detail": "You do not have permission to update this object."},
+                            status=status.HTTP_403_FORBIDDEN)
+
+
+class ShareFilePostDestroyAPI(DestroyAPIView):
+    serializer_class = ShareFilePostSerializer
+    queryset = ShareFilePost.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_destroy(self, instance):
+        if self.request.user == instance.user:
+            instance.delete()
+        else:
+            return Response({"detail": "You do not have permission to delete this object."},
+                            status=status.HTTP_403_FORBIDDEN)
+
+
+class CommentFilePostPostAPI(ListCreateAPIView):
+    serializer_class = CommentFilePostSerializer
+    queryset = CommentFilePost.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class CommentFilePostListAPI(ListAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = CommentFilePostSerializer
+    queryset = CommentFilePost.objects.all()
+
+
+class CommentFilePostRetrieveAPI(RetrieveAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = CommentFilePostSerializer
+    queryset = CommentFilePost.objects.all()
+
+
+class CommentFilePostUpdateAPI(UpdateAPIView):
+    serializer_class = CommentFilePostSerializer
+    queryset = CommentFilePost.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_update(self, serializer):
+        instance = self.get_object()
+
+        if self.request.user == instance.user:
+            serializer.save()
+        else:
+            return Response({"detail": "You do not have permission to update this object."},
+                            status=status.HTTP_403_FORBIDDEN)
+
+
+class CommentFilePostDestroyAPI(DestroyAPIView):
+    serializer_class = CommentFilePostSerializer
+    queryset = CommentFilePost.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_destroy(self, instance):
+        if self.request.user == instance.user:
+            instance.delete()
+        else:
+            return Response({"detail": "You do not have permission to delete this object."},
+                            status=status.HTTP_403_FORBIDDEN)
+
+
+class LikeFilePostPostAPI(ListCreateAPIView):
+    serializer_class = LikeFilePostSerializer
+    queryset = LikeFilePost.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class LikeFilePostListAPI(ListAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = LikeFilePostSerializer
+    queryset = LikeFilePost.objects.all()
+
+
+class LikeFilePostRetrieveAPI(RetrieveAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = LikeFilePostSerializer
+    queryset = LikeFilePost.objects.all()
+
+
+class LikeFilePostUpdateAPI(UpdateAPIView):
+    serializer_class = LikeFilePostSerializer
+    queryset = LikeFilePost.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_update(self, serializer):
+        instance = self.get_object()
+
+        if self.request.user == instance.user:
+            serializer.save()
+        else:
+            return Response({"detail": "You do not have permission to update this object."},
+                            status=status.HTTP_403_FORBIDDEN)
+
+
+class LikeFilePostDestroyAPI(DestroyAPIView):
+    serializer_class = LikeFilePostSerializer
+    queryset = LikeFilePost.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_destroy(self, instance):
+        if self.request.user == instance.user:
+            instance.delete()
+        else:
+            return Response({"detail": "You do not have permission to delete this object."},
+                            status=status.HTTP_403_FORBIDDEN)
+
+
+class ViewFilePostPostAPI(ListCreateAPIView):
+    serializer_class = ViewFilePostSerializer
+    queryset = ViewFilePost.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class ViewFilePostListAPI(ListAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = ViewFilePostSerializer
+    queryset = ViewFilePost.objects.all()
+
+
+class ViewFilePostRetrieveAPI(RetrieveAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = ViewFilePostSerializer
+    queryset = ViewFilePost.objects.all()
+
+
+class ViewFilePostUpdateAPI(UpdateAPIView):
+    serializer_class = ViewFilePostSerializer
+    queryset = ViewFilePost.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_update(self, serializer):
+        instance = self.get_object()
+
+        if self.request.user == instance.user:
+            serializer.save()
+        else:
+            return Response({"detail": "You do not have permission to update this object."},
+                            status=status.HTTP_403_FORBIDDEN)
+
+
+class ViewFilePostDestroyAPI(DestroyAPIView):
+    serializer_class = ViewFilePostSerializer
+    queryset = ViewFilePost.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_destroy(self, instance):
+        if self.request.user == instance.user:
+            instance.delete()
+        else:
+            return Response({"detail": "You do not have permission to delete this object."},
+                            status=status.HTTP_403_FORBIDDEN)
+
+
+class CommentRepliesView(ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = CommentFilePostSerializer
+
+    def get_queryset(self):
+        parent_comment_id = self.kwargs['parent_comment_id']
+        return CommentFilePost.objects.filter(parent_comment_id=parent_comment_id)
